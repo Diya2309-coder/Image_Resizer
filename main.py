6ad63669aebde586a1750f5f1c3f17e08e57b922
Diya2309-coder <diyapatel82008@gmail.com>
2023-06-24 11:51:36 +0530

    make change

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import cv2
 
-src_img= cv2.imread("img.png")
+source = "img.png"
+destination = "newimg.jpeg"
+#percent by which the image is resized
+scale_percent = 50
+
+
+src_img= cv2.imread(source)
 # cv2.imshow('Displaying Images', image)
 # cv2.waitKey(0)
 
-#percent by which the image is resized
-scale_percent = 50
 
 #calculate the 50 percent of the original dimensions
 new_width = int(src_img.shape[1] * scale_percent/100)
@@ -14,5 +18,5 @@ new_height = int(src_img.shape[0] * scale_percent/100)
 #resize image
 output = cv2.resize(src_img, (new_width, new_height))
 
-cv2.imwrite('newimg.png', output)
+cv2.imwrite(destination, output)
 cv2.waitKey(0)
