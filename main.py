d311611f0004cb3042ffb430b9d45a0a359163e0
Diya2309-coder <diyapatel82008@gmail.com>
2023-06-24 11:35:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+
+src_img= cv2.imread("img.png")
+# cv2.imshow('Displaying Images', image)
+# cv2.waitKey(0)
+
+#percent by which the image is resized
+scale_percent = 50
+
+#calculate the 50 percent of the original dimensions
+new_width = int(src_img.shape[1] * scale_percent/100)
+new_height = int(src_img.shape[0] * scale_percent/100)
+
+#resize image
+output = cv2.resize(src_img, (new_width, new_height))
+
+cv2.imwrite('newimg.png', output)
+cv2.waitKey(0)
